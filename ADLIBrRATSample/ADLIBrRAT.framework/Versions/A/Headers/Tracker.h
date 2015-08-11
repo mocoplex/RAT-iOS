43f5a31a772c0b58f083ccce31fda41bc56709a2
gskang <gskang@mocoplex.com>
2015-08-11 14:20:12 +0900

    Update SDK  v.1.0.4
    - updated ADLIBrRAT.framework v.1.0.4
    - removed AdSupport.framework

--- ADLIBrRATSample/ADLIBrRAT.framework/Versions/A/Headers/Tracker.h
+++ ADLIBrRATSample/ADLIBrRAT.framework/Versions/A/Headers/Tracker.h
@@ -5,25 +5,25 @@
 //  Created by YongSun Kim on 2014. 7. 16..
 //  Copyright (c) 2014년 mocoplex. All rights reserved.
 //
+//  Tracker version 1.0.4
+//  Build time pm 16:00 07.21.2015
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-@interface Tracker : NSObject
-{
-    NSString *user;
-    BOOL debugMode;
+@interface Tracker : NSObject {
+
 }
 
 @property (nonatomic, strong) NSString* user;
 @property (nonatomic) BOOL debugMode;
 
 + (NSString *)getVersion;
-
 + (Tracker *)sharedSingletonClass;
 
-- (void)initialize:(NSString *) _ratApiKey;
-- (void)tagInit:(NSURL *) url;
+- (void)initialize:(NSString *)_ratApiKey;
+
+- (void)tagInit:(NSURL *)url;
 
 - (void)view:(NSDictionary *)vdic;
 - (void)cart:(NSDictionary *)vdic;
@@ -33,4 +33,5 @@
 
 - (BOOL)parseWebViewRequest:(UIWebView*)wv with:(NSURLRequest *)request;
 - (void)criteo_event:(NSArray*)events;
+
 @end
